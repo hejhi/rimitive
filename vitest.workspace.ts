30a4ac8f7cef118b98efba3b571e56ae0b29bca7
Henry Ivry <henryivry@macbookpro.mynetworksettings.com>
2025-07-02 16:03:10 -0400

    add lint to check

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,7 +1,6 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-  './packages/adapter-store-react/vitest.config.ts',
   './packages/benchmarks/vitest.config.ts',
   './packages/core/vitest.config.ts',
   './packages/frameworks/vitest.config.ts',
