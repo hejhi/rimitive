05b637115e6388d739c47a7692574edb3374ea7b
Henry Ivry <henryivry@hibook.local>
2025-07-27 14:25:30 -0700

    fix effect type

--- packages/signals/src/effect.ts
+++ packages/signals/src/effect.ts
@@ -1,12 +1,12 @@
 import { CONSTANTS } from './constants';
 import type { SignalContext } from './context';
-import { Edge, ScheduledNode, StatefulNode } from './types';
+import { DisposableNode, Edge, ScheduledNode, StatefulNode } from './types';
 import type { LatticeExtension } from '@lattice/lattice';
 import { createNodePoolHelpers } from './helpers/node-pool';
 import { createSourceCleanupHelpers } from './helpers/source-cleanup';
 import { createScheduledConsumerHelpers } from './helpers/scheduled-consumer';
 
-export interface EffectInterface extends ScheduledNode, StatefulNode {
+export interface EffectInterface extends ScheduledNode, StatefulNode, DisposableNode {
   __type: 'effect';
   _callback(): void;
   dispose(): void;
