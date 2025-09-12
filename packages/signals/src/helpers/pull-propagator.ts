69c4884e737a195dc8dead898593c69810327d08
Henry Ivry <henryivry@hibook.local>
2025-09-12 23:04:27 +0200

    fix bug

--- packages/signals/src/helpers/pull-propagator.ts
+++ packages/signals/src/helpers/pull-propagator.ts
@@ -85,17 +85,18 @@ export function createPullPropagator(ctx: GlobalContext & { graphEdges: GraphEdg
         
         if (pStatus === STATUS_DIRTY) {
           if (recomputeNode(node) && parent) parent.flags = STATUS_DIRTY;
+          node.deferredParent = undefined;
+          current = parent;
         } else if (pStatus === STATUS_PENDING && 'compute' in producer) {
           // Single PENDING dependency - defer without saving position
           producer.deferredParent = node;
           current = producer;
-          continue;
         } else {
           // Single clean dependency
           node.flags = 0;
+          node.deferredParent = undefined;
+          current = parent;
         }
-        node.deferredParent = undefined;
-        current = parent;
         continue;
       }
 
