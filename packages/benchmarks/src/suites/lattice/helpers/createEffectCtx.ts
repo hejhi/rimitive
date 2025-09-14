6268847ab52ac8eda7849d75fcc23e539b8fc03d
Henry Ivry <henryivry@hibook.local>
2025-09-14 17:24:09 +0200

    fix bench types

--- packages/benchmarks/src/suites/lattice/helpers/createEffectCtx.ts
+++ packages/benchmarks/src/suites/lattice/helpers/createEffectCtx.ts
@@ -1,32 +1,15 @@
-import { GlobalContext } from "@lattice/signals/api";
 import { createBaseContext } from "@lattice/signals/context";
-import { EffectContext } from "@lattice/signals/effect";
 import { createGraphEdges } from "@lattice/signals/helpers/graph-edges";
-import { createNodeScheduler, NodeScheduler } from "@lattice/signals/helpers/node-scheduler";
+import { createNodeScheduler } from "@lattice/signals/helpers/node-scheduler";
 import { createPushPropagator } from "@lattice/signals/helpers/push-propagator";
-import { SignalContext } from "@lattice/signals/signal";
 
+export function createEffectContext(){
+  const ctx = createBaseContext();
 
-export function createEffectContext(): GlobalContext &
-  SignalContext &
-  EffectContext {
-  const baseCtx = createBaseContext();
-
-  // Create helpers with their dependencies
-  const graphEdges = createGraphEdges();
-  const pushPropagator = createPushPropagator();
-
-  const ctx = {
-    ...baseCtx,
-    graphEdges,
-    pushPropagator,
-    nodeScheduler: null as unknown as NodeScheduler, // Will be set below
+  return {
+    ctx,
+    graphEdges: createGraphEdges(),
+    push: createPushPropagator(),
+    nodeScheduler: createNodeScheduler(ctx),
   };
-
-  // Now create nodeScheduler with the same ctx object
-  const nodeScheduler = createNodeScheduler(ctx);
-
-  ctx.nodeScheduler = nodeScheduler;
-
-  return ctx;
 }
