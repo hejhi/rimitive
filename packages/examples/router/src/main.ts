ff97325579efd68ea5c3e90961124c73d0275ef1
Henry Ivry <henryivry@hibook.local>
2025-11-19 15:37:49 -0500

    update examples

--- packages/examples/router/src/main.ts
+++ packages/examples/router/src/main.ts
@@ -7,14 +7,14 @@ import { Product } from './pages/Product';
 import { NotFound } from './pages/NotFound';
 
 // Define the application routes
-const App = router.route('/', AppLayout({}))(
-  router.route('', Home({}))(),
-  router.route('about', About({}))(),
-  router.route('products', Products({}))(
-    router.route(':id', Product({}))()
+const App = router.route('/', AppLayout())(
+  router.route('', Home())(),
+  router.route('about', About())(),
+  router.route('products', Products())(
+    router.route(':id', Product())()
   ),
   // Catch-all route for 404
-  router.route('*', NotFound({}))()
+  router.route('*', NotFound())()
 );
 
 // Mount the app to the #app container
