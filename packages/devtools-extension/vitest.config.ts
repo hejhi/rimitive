353ff2104d844c53c430c88cd70d8e67be76f01d
Henry Ivry <henryivry@hibook.local>
2025-12-11 22:21:43 -0500

    fix check for devtools

--- packages/devtools-extension/vitest.config.ts
+++ packages/devtools-extension/vitest.config.ts
@@ -5,11 +5,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    pool: 'threads',
-    poolOptions: {
-      threads: {
-        singleThread: true,
-      },
+    sequence: {
+      concurrent: false,
     },
   },
   resolve: {
